39925449f1bed0e6098aaf437dbb03e6e58ab37e
AlessioBottin <alessiobottin2002@gmail.com>
2022-01-17 12:23:07 +0100

    scaffolding and styles.scss

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <header>
+            
+        </header>
+
+        <div class="jumbo">
+            <div class="container">
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/utilities.scss';
+@import '../styles/variables.scss';
+
+section {
+    background-image: url(../assets/img/);
+}
+
+</style>
